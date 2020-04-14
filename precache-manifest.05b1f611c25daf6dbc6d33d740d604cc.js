1372a70e0fb806afc6c8213a0bf743d4d5029225
gargdivyam327 <gargdivyam327@gmail.com>
2020-04-14 12:44:57 +0530

    Updates

---
+++ precache-manifest.05b1f611c25daf6dbc6d33d740d604cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcda257ba74f4935313262c787a628c",
+    "url": "/FoodRecipe-Website/index.html"
+  },
+  {
+    "revision": "03935ce8e308dc8fa8ec",
+    "url": "/FoodRecipe-Website/static/css/main.ef9b938d.chunk.css"
+  },
+  {
+    "revision": "5ad9eb2efe71a3fc5d2e",
+    "url": "/FoodRecipe-Website/static/js/2.0f9e70c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FoodRecipe-Website/static/js/2.0f9e70c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03935ce8e308dc8fa8ec",
+    "url": "/FoodRecipe-Website/static/js/main.25b9b53b.chunk.js"
+  },
+  {
+    "revision": "7af463777078fec51e4f",
+    "url": "/FoodRecipe-Website/static/js/runtime-main.eb71c7cc.js"
+  }
+]);
